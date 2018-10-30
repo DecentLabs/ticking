388162ae618f468e03017bb9627134e46df99b8b
brigittaforrai <forraibrigi@gmail.com>
2018-10-30 18:20:05 +0100

    styles

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ let equalizer = document.querySelector('.equalizer')
 let style = document.createElement('style')
 
 let bars = [];
-let barNums = 22;
+let barNums = 25;
 
 let random = function (multi) {
   return (Math.random() * multi) + 'px'
@@ -13,8 +13,8 @@ for (let i=0; i < barNums; i++) {
   bar.classList.add('eq-bar')
   equalizer.appendChild(bar)
 
-  let multi = i < 3  || i > barNums.length - 4 ? 75 : 200
-  bar.style.animation = `move${i} ${Math.random() * 2.5 + 0.6}s infinite alternate`
+  let multi = i < 3  || i > barNums.length - 4 ? 70 : 140
+  bar.style.animation = `move${i} ${Math.random() * 3 + 0.8}s infinite alternate`
   let animation = `@keyframes move${i} { 0% { height: ${random(multi)}; } 25% { height: ${random(multi)}; } 50% { height: ${random(multi)}; } 75% { height: ${random(multi)}; } 100% { height: ${random(multi)}; } }`
   style.innerHTML = style.innerHTML + animation
 }
